c7484b608a59eaa53b0659d999df8729d725ebbd
Ali Othman <alioth840@gmail.com>
2025-01-02 15:09:21 +0100

    Add createTask function with use server directive for task creation

--- src/app/task/add/page.tsx
+++ src/app/task/add/page.tsx
@@ -1,4 +1,19 @@
 import Link from "next/link"
+import prisma from "@/utils/db";
+
+async function createTask(formData:FormData){
+    "use server";
+    const title = formData.get("title")?.toString()
+    const description = formData.get("description")?.toString()
+
+    if(!title || !description) return console.log("Required")
+
+    await prisma.task.create({
+        data:{
+            title,description
+        }
+    })
+}
 
 function AddTaskPage() {
     return (
@@ -8,7 +23,7 @@ function AddTaskPage() {
             </Link>
             <div className="w-2/3 mx-auto rounded-md p-5 bg-slate-800 bottom-2 border-gray-300">
                 <h1 className="mb-7 font-bold text-3xl">Add Your Task</h1>
-                <form action="" className="flex flex-col gap-6">
+                <form action={createTask} className="flex flex-col gap-6">
                     <input type="text" placeholder="Task Title" name="title" className="p-2 text-xl rounded-md text-gray-950"/>
                     <textarea name="description" rows={5} placeholder="Task Description" className="p-2 text-xl rounded-md text-gray-950 resize-none"/>
                     <button type="submit" className="bg-cyan-300 text-black font-semibold text-xl rounded-md p-3 transition-colors" >Add Task</button>
