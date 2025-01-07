bedefcfbc668c6752fdc217de0b98ec75e0cce3b
Ali Othman <alioth840@gmail.com>
2025-01-07 12:20:59 +0100

    Display tasks in a table on the HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link"
+import prisma from "@/utils/db"
 
-function HomePage() {
+async function  HomePage() {
+  const tasks = await prisma.task.findMany();
   return (
     <section>
       <h1 className='text-4xl font-semibold'>Tasks List App</h1>
@@ -9,6 +11,28 @@ function HomePage() {
             Add Task
         </Link>
       </div>
+      <table className="table w-full text-left mt-5">
+        <thead className="border-t-2 border-b-2 border-gray-300 text-xl">
+          <tr>
+            <th className="p-3">#</th>
+            <th>Task Title</th>
+            <th>Task Status</th>
+            <th>Task Details</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tasks.map((task,index)=>(
+            <tr key={task.id} className="border-b border-gray-500">
+              <td className="p-3">{index + 1}</td>
+              <td>{task.title}</td>
+              <td>{task.status.toString()}</td>
+              <td><Link href={`/task/${task.id}`} className="bg-blue-600 hover:bg-blue-800 transition-colors text-white rounded-md p-2">Details</Link></td>
+
+            </tr>
+          ))}
+        </tbody>
+
+      </table>
     </section>
   )
 }
