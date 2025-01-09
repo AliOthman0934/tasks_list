88ef3335a8a979c977fd71bb7d09e72df610dfc0
Ali Othman <alioth840@gmail.com>
2025-01-09 15:54:40 +0100

    Install Zod for validation and React Toastify for notifications, refactor AddTaskForm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import "react-toastify/dist/ReactToastify.css"
+import { ToastContainer } from "react-toastify";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <ToastContainer theme="colored" position="top-center"/>
         <main className="container mx-auto p-10">
           {children}
         </main>
