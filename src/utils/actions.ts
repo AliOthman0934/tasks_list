79bc1705057df37f9fe5ec4ac8fd74f75f93b8b7
Ali Othman <alioth840@gmail.com>
2025-01-10 19:24:10 +0100

    Ensured compatibility with the `createTask` server-side function.

--- src/utils/actions.ts
+++ src/utils/actions.ts
@@ -2,17 +2,19 @@
 import prisma from "@/utils/db";
 import { redirect } from "next/navigation";
 import { revalidatePath } from "next/cache";
+import { createTaskDto } from "./dtos";
 
 
-export async function createTask(formData:FormData){
-const title = formData.get("title")?.toString()
-const description = formData.get("description")?.toString()
+export async function createTask(newTask:createTaskDto){
+// const title = formData.get("title")?.toString()
+// const description = formData.get("description")?.toString()
 
-if(!title || !description) return console.log("Required")
+if(!newTask.title || !newTask.description) return console.log("Required")
 
 await prisma.task.create({
     data:{
-        title,description
+        title :newTask.title,
+        description :newTask.description
     }
 })
 revalidatePath("/")
