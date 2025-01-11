57c46acf1c9d3ce626e39672482de5f6a3d95240
Ali Othman <alioth840@gmail.com>
2025-01-11 18:15:54 +0100

    Ensured validation with the `createTask` server-side function.

--- src/utils/validationSchema.ts
+++ src/utils/validationSchema.ts
@@ -1,4 +1,3 @@
-
 import {z} from "zod"
 
 export const createTaskSchema = z.object({
@@ -6,13 +5,12 @@ export const createTaskSchema = z.object({
         required_error: "Title is required",
         invalid_type_error: "Title should be of type string"
     })
-    .min(2,{message: "Title should be at least 2 characters long"})
+    .min(2,{message: "Title should be at least 2 characters"})
     .max(200,{message : "Title should be less than 200 characters"}),
 
     description: z.string({
         required_error: "Description is required",
         invalid_type_error: "Description should be of type string"
     })
-    .min(4,{message: "Description should be at least 4 characters long"})
-    
+    .min(2,{message: "Description should be at least 4 characters"})
 })
